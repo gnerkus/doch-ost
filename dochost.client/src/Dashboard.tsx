bd29968930d7893260965fafbb8f2983b6492cdd
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-20 13:47:44 +0100

    configure react query
    configure tailwind for UI

---
+++ dochost.client/src/Dashboard.tsx
@@ -0,0 +1,30 @@
+﻿import {useQuery} from "@tanstack/react-query";
+
+function Dashboard() {
+    const { isPending, error, data, isFetching} = useQuery({
+        queryKey: ['weatherforecast'],
+        queryFn: async () => {
+            const response = await fetch(
+                "weatherforecast"
+            )
+            return await response.json();
+        }
+    });
+
+    if (isPending) return 'Loading...'
+
+    if (error) return 'An error has occurred: ' + error.message
+
+    return (
+        <>
+            <h1>Doch Ost</h1>
+            {data.map((forecast: any) => {
+                return (
+                    <div>{forecast.summary}</div>
+                )
+            })}
+        </>
+    )
+}
+
+export default Dashboard;
