4ffa2f4297c8402fe9f897208aa621ca6db6d3f4
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-21 18:57:48 +0100

    utilize axios for queries to enable interceptor modification

--- dochost.client/src/core/http/api.ts
+++ dochost.client/src/core/http/api.ts
@@ -1,17 +1,30 @@
 ﻿import {LoginInput} from "../contracts/auth.ts";
+import {axiosInstance} from "./axiosInstance.ts";
 
 export const login = async (requestBody: LoginInput) => {
-  const response = await fetch("api/login", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json"
-      },
-      body: JSON.stringify(requestBody)
-  });
+    const response = await axiosInstance.post("api/login", requestBody, {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
 
-  if (!response.ok) {
+  if (response.status !== 200) {
       throw new Error("Network response was not ok")
   }
 
-  return await response.json();
+  return response.data;
+}
+
+export const fetchDocuments = async () => {
+    const response = await axiosInstance.get("api/documents", {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (response.status !== 200) {
+        throw new Error("Network response was not ok")
+    }
+
+    return response.data;
 }
