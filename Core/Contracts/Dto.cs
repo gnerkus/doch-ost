a0f3260da900e9808ebca84d202d2e8e8a50331f
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-21 13:15:19 +0100

    add display name to document info table

---
+++ Core/Contracts/Dto.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Contracts
+{
+    public record DocumentInfoDto
+    {
+        public Guid Id { get; init; }
+        public required string FileName { get; init; }
+        public required string FileExt { get; init; }
+        public int DownloadCount { get; init; }
+        public DateTime? CreatedAt { get; init; }
+        public DateTime? UpdatedAt { get; init; }
+
+    }
+}
