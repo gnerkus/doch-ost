5a0e430b27f87ab7f9c0f12f87f4865db776e5c2
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-23 12:35:56 +0100

    add preview url to document info

---
+++ Data/Migrations/20241123113120_AddPreviewUrlToDocInfo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreviewUrlToDocInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PreviewUrl",
+                table: "DocumentInfos",
+                type: "TEXT",
+                maxLength: 60,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreviewUrl",
+                table: "DocumentInfos");
+        }
+    }
+}
