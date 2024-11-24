88e833156c3fc07995721ee281a8b0ccda959be7
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-24 10:51:04 +0100

    add loading animation

--- dochost.client/src/core/components/LoadingIcon.tsx
+++ dochost.client/src/core/components/LoadingIcon.tsx
@@ -2,14 +2,14 @@
     size: number
 }
 
-function LoadingIcon({ size}: LoadingIconProps) {
+function LoadingIcon({size}: LoadingIconProps) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}
-             viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2}
-             strokeLinecap="round" strokeLinejoin="round"
-             className="text-slate-600">
+        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24"
+             fill="none"
+             stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"
+             className="text-slate-600 animate-spin">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M19.95 11a8 8 0 1 0 -.5 4m.5 5v-5h-5"/>
+            <path d="M12 3a9 9 0 1 0 9 9"/>
         </svg>
     );
 }
---
+++ dochost.client/src/core/utils.ts
@@ -0,0 +1,10 @@
+﻿export const blobToBase64 = function (blob: Blob) {
+    return new Promise((resolve: (value: string | ArrayBuffer | null) => void) => {
+        const reader = new FileReader();
+        reader.onload = function () {
+            const dataUrl = reader.result;
+            resolve(dataUrl);
+        };
+        reader.readAsDataURL(blob);
+    });
+}
